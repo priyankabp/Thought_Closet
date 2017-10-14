75e9afbcbd8c9ead503ccbc00cdf18c81bf9c890
Priyanka Phapale <pbphapale@gmail.com>
2017-10-14 15:45:48 -0400

    Fixed post.php footer bug

--- post.php
+++ post.php
@@ -151,10 +151,4 @@
       </div>
     </section>
 
-    <?php require_once ('include/footer.php')  ?>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+  <?php require_once ('include/footer.php')  ?>
