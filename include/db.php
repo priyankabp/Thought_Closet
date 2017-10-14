0aec1dd7098c94c2f7b3489cc00d80080b001afa
Priyanka Phapale <pbphapale@gmail.com>
2017-10-14 16:43:08 -0400

    Add DB connection

---
+++ include/db.php
@@ -0,0 +1,13 @@
+<?php
+
+	$db['db_host'] = 'localhost';
+	$db['db_user'] = 'root';
+	$db['db_password'] = '';
+	$db['db_name'] = 'cms';
+
+	foreach ($db as $key => $value) {
+		define(strtoupper($key),$value);
+	}
+	$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+	
+?>
