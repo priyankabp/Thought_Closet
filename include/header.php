d51960f856100034625f89af8ae406f8f35f5f14
Priyanka Phapale <pbphapale@gmail.com>
2017-10-14 17:11:27 -0400

    Change categories menu in header from static to dynamic

--- include/header.php
+++ include/header.php
@@ -22,8 +22,20 @@
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-list-alt" aria-hidden="true"></i> Categories<span class="caret"></span></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Category 1</a></li>
-                <li><a href="#">Category 2</a></li>
+                <?php
+                    $query = "SELECT * FROM categories";
+                    $run = mysqli_query($connection,$query);
+                    if(mysqli_num_rows($run) > 0){
+                      while ($row = mysqli_fetch_array($run)) {
+                        $category = ucfirst($row['category']);
+                        $id = $row['id'];
+                        echo "<li><a href='index.php?category=".$id."'>$category</a></li>";
+                      }
+                    }
+                    else{
+                      echo "<li><a href='#'>No Categories Yet</a></li>";
+                    }
+                  ?>
               </ul>
             </li>
 
