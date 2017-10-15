8fde72d8297c5d9955a740a14869d87e850bb43f
Priyanka Phapale <pbphapale@gmail.com>
2017-10-14 20:39:45 -0400

    Change popular and recent posts dynamic

--- include/right_sidebar.php
+++ include/right_sidebar.php
@@ -10,72 +10,74 @@
             <div class="widgets"><!-- Popular Posts widgets open -->
               <div class="popular">
                 <h4> Popular Posts</h4>
-                <hr>
-                <div class="row">
-                  <div class="col-xs-4">
-                    <a href="#"><img src="images/slider-img1.jpg" alt="Image 1"></a>
-                  </div>
-                  <div class="col-xs-8 details">
-                    <a href="#"><h4>This is heading for post</h4></a>
-                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> 10 January, 2017</p>
-                  </div>
-                </div>
+                <?php
+                  $popular_query = "SELECT * FROM posts WHERE status = 'publish' ORDER BY views DESC LIMIT 5";
+                  $popular_run = mysqli_query($connection,$popular_query);
+                  if (mysqli_num_rows($popular_run) >0 ) {
+                    while ($popular_row = mysqli_fetch_array($popular_run)) {
+                      $popular_id = $popular_row['id'];
+                      $popular_date = getdate($popular_row['date']);
+                      $popular_day = $popular_date['mday'];
+                      $popular_month = $popular_date['month'];
+                      $popular_year = $popular_date['year'];
+                      $popular_title = $popular_row['title'];
+                      $popular_image = $popular_row['image'];
 
-                <div class="row">
-                  <div class="col-xs-4">
-                    <a href="#"><img src="images/slider-img1.jpg" alt="Image 1"></a>
+                  ?>
+                  <hr>
+                  <div class="row">
+                    <div class="col-xs-4">
+                      <a href="post.php?post_id=<?php echo $popular_id; ?>"><img src="images/<?php echo $popular_image;?>" alt="Image 1"></a>
+                    </div>
+                    <div class="col-xs-8 details">
+                      <a href="post.php?post_id=<?php echo $popular_id; ?>"><h5><?php echo $popular_title;?></h5></a>
+                      <p><i class="fa fa-clock-o" aria-hidden="true"></i> <?php echo $popular_day." ".$popular_month." , ".$popular_year;?></p>
+                    </div>
                   </div>
-                  <div class="col-xs-8 details">
-                    <a href="#"><h4>This is heading for post</h4></a>
-                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> 10 January, 2017</p>
-                  </div>
-                </div>
-
-                <div class="row">
-                  <div class="col-xs-4">
-                    <a href="#"><img src="images/slider-img1.jpg" alt="Image 1"></a>
-                  </div>
-                  <div class="col-xs-8 details">
-                    <a href="#"><h4>This is heading for post</h4></a>
-                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> 10 January, 2017</p>
-                  </div>
-                </div>
+                  <?php
+                    }
+                   }
+                   else{
+                    echo "<h3>No Post Available</h3>";
+                   }
+                  ?>
               </div>
             </div><!-- Popular Posts widgets closed -->
 
             <div class="widgets"><!-- Recent Posts widgets open -->
               <div class="popular">
                 <h4> Recent Posts</h4>
-                <hr>
-                <div class="row">
-                  <div class="col-xs-4">
-                    <a href="#"><img src="images/slider-img1.jpg" alt="Image 1"></a>
-                  </div>
-                  <div class="col-xs-8 details">
-                    <a href="#"><h4>This is heading for post</h4></a>
-                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> 10 January, 2017</p>
-                  </div>
-                </div>
+                <?php
+                  $recent_query = "SELECT * FROM posts WHERE status = 'publish' ORDER BY id DESC LIMIT 5";
+                  $recent_run = mysqli_query($connection,$recent_query);
+                  if (mysqli_num_rows($recent_run) >0 ) {
+                    while ($recent_row = mysqli_fetch_array($recent_run)) {
+                      $recent_id = $recent_row['id'];
+                      $recent_date = getdate($recent_row['date']);
+                      $recent_day = $recent_date['mday'];
+                      $recent_month = $recent_date['month'];
+                      $recent_year = $recent_date['year'];
+                      $recent_title = $recent_row['title'];
+                      $recent_image = $recent_row['image'];
 
-                <div class="row">
-                  <div class="col-xs-4">
-                    <a href="#"><img src="images/slider-img1.jpg" alt="Image 1"></a>
+                  ?>
+                  <hr>
+                  <div class="row">
+                    <div class="col-xs-4">
+                      <a href="post.php?post_id=<?php echo $recent_id; ?>"><img src="images/<?php echo $recent_image;?>" alt="Image 1"></a>
+                    </div>
+                    <div class="col-xs-8 details">
+                      <a href="post.php?post_id=<?php echo $recent_id; ?>"><h5><?php echo $recent_title;?></h5></a>
+                      <p><i class="fa fa-clock-o" aria-hidden="true"></i> <?php echo $recent_day." ".$recent_month." , ".$recent_year;?></p>
+                    </div>
                   </div>
-                  <div class="col-xs-8 details">
-                    <a href="#"><h4>This is heading for post</h4></a>
-                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> 10 January, 2017</p>
-                  </div>
-                </div>
-
-                <div class="row">
-                  <div class="col-xs-4">
-                    <a href="#"><img src="images/slider-img1.jpg" alt="Image 1"></a>
-                  </div>
-                  <div class="col-xs-8 details">
-                    <a href="#"><h4>This is heading for post</h4></a>
-                    <p><i class="fa fa-clock-o" aria-hidden="true"></i> 10 January, 2017</p>
-                  </div>
-                </div>
+                  <?php
+                    }
+                   }
+                   else{
+                    echo "<h3>No Post Available</h3>";
+                   }
+                  ?>
               </div>
             </div><!-- Recent Posts widgets closed -->
 
