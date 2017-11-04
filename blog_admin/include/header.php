a7749c43cb875a8f886697ca7494cdbe4ca272f4
Priyanka Phapale <pbphapale@gmail.com>
2017-11-04 11:28:45 -0400

    Change UI for author and admin

--- blog_admin/include/header.php
+++ blog_admin/include/header.php
@@ -1,3 +1,7 @@
+<?php
+  $session_role2 = $_SESSION['role'];
+  $session_username2 = $_SESSION['username'];
+?>
 <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container-fluid">
           <div class="navbar-header">
@@ -12,8 +16,13 @@
 
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
             <ul class="nav navbar-nav navbar-right">
+              <li><a href="">Welcome : <i class="fa fa-user" aria-hidden="true"></i> <?php echo ucfirst($session_username2);?></a></li>
               <li><a href="add-post.php"><i class="fa fa-plus-square" aria-hidden="true"></i> Add Post</a></li>
+              <?php
+                if ($session_role2 == 'admin') {
+              ?>
               <li><a href="add-user.php"><i class="fa fa-user-plus" aria-hidden="true"></i> Add User</a></li>
+              <?php } ?>
               <li><a href="profile.php"><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>
               <li><a href="logout.php"><i class="fa fa-power-off" aria-hidden="true"></i> Log Out</a></li>
             </ul>
--- blog_admin/include/left_sidebar.php
+++ blog_admin/include/left_sidebar.php
@@ -1,25 +1,38 @@
+<?php
+  $session_role1 = $_SESSION['role'];
+  $get_comment = "SELECT * FROM comments WHERE status = 'pending'";
+  $get_comment_run = mysqli_query($connection,$get_comment);
+  $num_of_rows = mysqli_num_rows($get_comment_run);
+?>
 <div class="list-group">
               <a href="index.php" class="list-group-item active">
                 <i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard
               </a>
               <a href="posts.php" class="list-group-item">
-                <span class="badge">14</span>
                 <i class="fa fa-file-text" aria-hidden="true"></i> All Posts
               </a>
               <a href="media.php" class="list-group-item">
-                <span class="badge">14</span>
                 <i class="fa fa-database" aria-hidden="true"></i> Media
               </a>
+
+              <?php
+                if ($session_role1 == 'admin') {
+              ?>
               <a href="comments.php" class="list-group-item">
-                <span class="badge">10</span>
+                <?php
+                  if($num_of_rows > 0){
+                    echo "<span class='badge'>$num_of_rows</span>";
+                  }
+                ?>
+                
+                
                 <i class="fa fa-comments" aria-hidden="true"></i> Comments
               </a>
               <a href="categories.php" class="list-group-item">
-                <span class="badge">5</span>
                 <i class="fa fa-list-alt" aria-hidden="true"></i> Categories
               </a>
               <a href="users.php" class="list-group-item">
-                <span class="badge">9</span>
                 <i class="fa fa-users" aria-hidden="true"></i> Users
               </a>
+              <?php } ?>
             </div>
